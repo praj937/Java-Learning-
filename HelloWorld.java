ad4088b85ce62b8a3eeb40fd6a4820def48e36c5
Prajwal S Hiremath <prajwal0937@gmail.com>
2026-09-17 20:29:45 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello,World!");
+        int a = 10;
+        int b = 20;
+        int result = a + b;
+        System.out.println("The sum of a and b is : "+ result);
+
+    }
+} 
