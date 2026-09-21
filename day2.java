29a80753364637ae8591a0d0b4fdbf1ce092e585
Prajwal S Hiremath <prajwal0937@gmail.com>
2026-09-21 21:52:47 +0530

    byte data type

--- day2.java
+++ day2.java
@@ -1,9 +1,18 @@
 class Geeks{
     public static void main(String[] args){
+        //Boolean
         boolean isJavaFun = true;
         boolean isFishTasty = false;
-        System.out.println("Is Java fun?" + isJavaFun);
-        System.out.println("Is Fish tasty?" + isFishTasty);
+        System.out.println("Is Java fun?" +isJavaFun);
+        System.out.println("Is Fish tasty?" +isFishTasty);
+
+        //Byte
+        byte age = 25;
+        byte temperature = -10;
+        System.out.println("Age:" +age);
+        System.out.println("Temperature:" +temperature);
+
+
 
     }
 }
