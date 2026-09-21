4588a32488f7d87cfc697074dad0310f63e5b01c
Prajwal S Hiremath <prajwal0937@gmail.com>
2026-09-21 21:32:34 +0530

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,9 @@
+class Geeks{
+    public static void main(String[] args){
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println("Is Java fun?" + isJavaFun);
+        System.out.println("Is Fish tasty?" + isFishTasty);
+
+    }
+}
